e1a5035d9c7e26f3084898df00688e92728c2e5a
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-06-30 15:07:52 +0700

    Create Config-V4-[2].lua

---
+++ Config-V4-[2].lua
@@ -0,0 +1,13 @@
+getgenv().Configuration = {
+        ['Enabled'] = true,
+        ['Enabled Challenge'] = false,
+        ['Summon The Beast'] = true,
+        ['PC Name'] = 'V4 [2]',
+        ['Delay'] = 0,
+        ['Leave Delay'] = 0,
+        ['EquipBest'] = true,
+        ['Leave At Wave'] = 31,
+        ['Leave Method'] = 1, -- 1 = Sell,  2 = Leave
+        ['TradingMode'] = false,
+        ['License'] = 'JameFarmOnTop',
+  };

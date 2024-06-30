9b3838b917a5c509b39736feaa7acc009db4929a
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-07-01 03:27:41 +0700

    Create Config 28-56-[1].lua

---
+++ Config 28-56-[1].lua
@@ -0,0 +1,13 @@
+getgenv().Configuration = {
+        ['Enabled'] = true,
+        ['Enabled Challenge'] = false,
+        ['Summon The Beast'] = true,
+        ['PC Name'] = '28/56 - 1',
+        ['Delay'] = 0,
+        ['Leave Delay'] = 0,
+        ['EquipBest'] = true,
+        ['Leave At Wave'] = 31,
+        ['Leave Method'] = 1, -- 1 = Sell,  2 = Leave
+        ['TradingMode'] = false,
+        ['License'] = 'JameFarmOnTop',
+  };

e23e3e27c70418f1340c8d6fe45d99279bc080f6
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-07-02 22:19:40 +0700

    Update Config V4 [3].lua

--- Config V4 [3].lua
+++ Config V4 [3].lua
@@ -8,13 +8,6 @@
         ['EquipBest'] = true,
         ['Leave At Wave'] = 31,
         ['Leave Method'] = 1, -- 1 = Sell,  2 = Leave
-        ['TradingMode'] = true,
+        ['TradingMode'] = false,
         ['License'] = 'JameFarmOnTop',
    };
-spawn(function()
-    while true do task.wait(1) 
-        pcall(function()
-            game:GetService('RunService'):Set3dRenderingEnabled(true)
-        end)
-    end;
-end)

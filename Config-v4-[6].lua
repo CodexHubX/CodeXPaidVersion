201e1e7f2ce8afcb0aa17756c48275b2d6a07ef3
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-08-01 03:40:55 +0700

    Create Config-v4-[6].lua

---
+++ Config-v4-[6].lua
@@ -0,0 +1,25 @@
+game:GetService('RunService'):Set3dRenderingEnabled(false)
+
+local AutoRejoinThread = task.spawn(function()
+   repeat 
+        task.wait() 
+    until game:IsLoaded();
+    task.wait(30);
+    if not game.Players.LocalPlayer.PlayerGui:FindFirstChild('Anime Defenders') then 
+        game:GetService('TeleportService'):Teleport(17017769292);
+   end;
+end);
+getgenv().Configuration = {
+        ['Enabled'] = true,
+        ['Enabled Challenge'] = false,
+        ['Summon The Beast'] = false,
+        ['PC Name'] = 'V4 [6]',
+        ['Delay'] = 0,
+        ['Leave Delay'] = 0,
+        ['EquipBest'] = true,
+        ['Leave At Wave'] = 31,
+        ['Leave Method'] = 1, -- 1 = Sell,  2 = Leave
+        ['TradingMode'] = false,
+        ['Main User'] = '',
+        ['License'] = 'JameFarmOnTop',
+  };

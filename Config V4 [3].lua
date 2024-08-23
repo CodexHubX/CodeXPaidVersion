bf527f95b3454eac93c5129990b639ca2c5d2102
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-08-23 14:04:50 +0700

    Config V4 [3].lua

--- Config V4 [3].lua
+++ Config V4 [3].lua
@@ -1,14 +1,14 @@
 game:GetService('RunService'):Set3dRenderingEnabled(false)
 
-local AutoRejoinThread = task.spawn(function()
-    repeat 
-        task.wait() 
-    until game:IsLoaded();
-    task.wait(120);
-    if not game.Players.LocalPlayer.PlayerGui:FindFirstChild('Anime Defenders') then 
-        game:GetService('TeleportService'):Teleport(17017769292);
-    end;
-end);
+--local AutoRejoinThread = task.spawn(function()
+--    repeat 
+--        task.wait() 
+--    until game:IsLoaded();
+--    task.wait(120);
+--    if not game.Players.LocalPlayer.PlayerGui:FindFirstChild('Anime Defenders') then 
+--        game:GetService('TeleportService'):Teleport(17017769292);
+--    end;
+--end);
 
 getgenv().Configuration = {
         ['Enabled'] = true,
@@ -20,8 +20,8 @@ getgenv().Configuration = {
         ['EquipBest'] = true,
         ['Leave At Wave'] = 31,
         ['Leave Method'] = 2, -- 1 = Sell,  2 = Leave
-        ['TradingMode'] = false,
-        ['Main User'] = '',
+        ['TradingMode'] = true,
+        ['Main User'] = 'TipsySnowLil',
         ['License'] = 'JameFarmOnTop',
         ['Roll Mythic'] = true,
         ['Roll Method'] = 2,
@@ -40,4 +40,4 @@ getgenv().Configuration = {
          ['Placement Distance'] = 15,
    };
 
---spawn(function() while task.wait(1) do pcall(function() game:GetService('RunService'):Set3dRenderingEnabled(true) end) end end)
+spawn(function() while task.wait(1) do pcall(function() game:GetService('RunService'):Set3dRenderingEnabled(true) end) end end)

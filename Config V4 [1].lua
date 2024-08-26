fcd9c84cd7772888c5ff0734776ddba795b354fa
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-08-26 12:52:57 +0700

    Update Config V4 [1].lua

--- Config V4 [1].lua
+++ Config V4 [1].lua
@@ -1,14 +1,14 @@
 game:GetService('RunService'):Set3dRenderingEnabled(false)
 
-local AutoRejoinThread = task.spawn(function()
-   repeat 
-        task.wait() 
-    until game:IsLoaded();
-    task.wait(120);
-    if not game.Players.LocalPlayer.PlayerGui:FindFirstChild('Anime Defenders') then 
-        game:GetService('TeleportService'):Teleport(17017769292);
-   end;
-end);
+--local AutoRejoinThread = task.spawn(function()
+--   repeat 
+--        task.wait() 
+--    until game:IsLoaded();
+--    task.wait(120);
+--    if not game.Players.LocalPlayer.PlayerGui:FindFirstChild('Anime Defenders') then 
+--        game:GetService('TeleportService'):Teleport(17017769292);
+--   end;
+--end);
 getgenv().Configuration = {
         ['Enabled'] = true,
         ['Enabled Challenge'] = false,
@@ -19,8 +19,8 @@ getgenv().Configuration = {
         ['EquipBest'] = true,
         ['Leave At Wave'] = 31,
         ['Leave Method'] = 2, -- 1 = Sell,  2 = Leave
-        ['TradingMode'] = false,
-        ['Main User'] = '',
+        ['TradingMode'] = true,
+        ['Main User'] = 'YungFishSoul',
         ['Reroll Farm Pro Max'] = true,
         ['License'] = 'JameFarmOnTop',
         ['Roll Mythic'] = true,
@@ -38,4 +38,4 @@ getgenv().Configuration = {
          },
          ['Placement Distance'] = 15,
   };
---spawn(function() while task.wait(1) do pcall(function() game:GetService('RunService'):Set3dRenderingEnabled(true) end) end end)
+spawn(function() while task.wait(1) do pcall(function() game:GetService('RunService'):Set3dRenderingEnabled(true) end) end end)

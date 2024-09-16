82d04cb8c06a4c8de1d1b2443d2701cd2cfb64ca
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-09-16 22:50:12 +0700

    Create Config-v4-[8].lua

---
+++ Config-v4-[8].lua
@@ -0,0 +1,41 @@
+game:GetService('RunService'):Set3dRenderingEnabled(false)
+
+local AutoRejoinThread = task.spawn(function()
+   repeat 
+        task.wait() 
+    until game:IsLoaded();
+    task.wait(120);
+    if not game.Players.LocalPlayer.PlayerGui:FindFirstChild('Anime Defenders') then 
+        game:GetService('TeleportService'):Teleport(17017769292);
+   end;
+end);
+getgenv().Configuration = {
+        ['Enabled'] = true,
+        ['Enabled Challenge'] = false,
+        ['Summon The Beast'] = false,
+        ['PC Name'] = 'V4 [8]',
+        ['Delay'] = 0,
+        ['Leave Delay'] = 0,
+        ['EquipBest'] = true,
+        ['Leave At Wave'] = 46,
+        ['Leave Method'] = 2, -- 1 = Sell,  2 = Leave
+        ['TradingMode'] = false,
+        ['Main User'] = '',
+        ['Reroll Farm Pro Max'] = true,
+        ['License'] = 'JameFarmOnTop',
+        ['Roll Mythic'] = true,
+        ['Roll Method'] = 2,
+        ['Auto Feed Mythic'] = true,
+         ['Roll Ticket'] = false,
+         ['Use Auto Sell'] = false,
+         ['Sell Config'] = {
+             ['Rare'] = true,
+             ['Epic'] = true,
+             ['Legendary'] = true,
+             ['Rare Shiny'] = true,
+             ['Epic Shiny'] = true,
+             ['Legendary Shiny'] = true,
+         },
+         ['Placement Distance'] = 25,
+  };
+--spawn(function() while task.wait(1) do pcall(function() game:GetService('RunService'):Set3dRenderingEnabled(true) end) end end)

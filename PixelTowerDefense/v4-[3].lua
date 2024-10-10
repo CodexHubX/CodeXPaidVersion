cd46d1fa67455be71a07258123fec700547e1a11
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-10-11 06:57:17 +0700

    Create v4-[3].lua

---
+++ PixelTowerDefense/v4-[3].lua
@@ -0,0 +1,8 @@
+    shared.Configs = {
+        Enabled = true,
+        RedeemCode = true,
+        Speed = 2,  
+        GithubUrl = 'https://raw.githubusercontent.com/CodexHubX/CodeXPaidVersion/refs/heads/main/ConfigPTD.lua',
+        JobID = '',
+        TradingMode = false,
+    };

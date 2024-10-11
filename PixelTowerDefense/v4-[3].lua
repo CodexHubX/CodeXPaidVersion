becf7fd3a4b28c17b6917a3cdba21619e8c2adbe
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-10-11 19:50:32 +0700

    Update v4-[3].lua

--- PixelTowerDefense/v4-[3].lua
+++ PixelTowerDefense/v4-[3].lua
@@ -3,6 +3,6 @@
         RedeemCode = true,
         Speed = 2,  
         GithubUrl = 'https://raw.githubusercontent.com/CodexHubX/CodeXPaidVersion/refs/heads/main/ConfigPTD.lua',
-        JobID = 'e031c183-2dcb-4192-a643-2983feccefca',
-        TradingMode = true,
+        JobID = '',
+        TradingMode = false,
     };

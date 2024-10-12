20320e8f222cf3aaa3dccc9f0eb78ddac59fdb6f
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-10-12 23:01:13 +0700

    Update v4-[8].lua

--- PixelTowerDefense/v4-[8].lua
+++ PixelTowerDefense/v4-[8].lua
@@ -7,8 +7,8 @@ shared.Configs = {
     JobID = '',
     TradingMode = false,
     AutoTrading = {
-        ['Enabled'] = false,
-        ['Main User'] = '',
-        ['JobID'] = '',
+        ['Enabled'] = true,
+        ['Main User'] = 'MoonPinkScar',
+        ['JobID'] = '4d86def9-49c7-4f29-8d27-43817443f832',
     },
 };

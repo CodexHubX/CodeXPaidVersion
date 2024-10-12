d86b090f175a1520f5ed6d764ccfe47e1ffd8992
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-10-12 22:53:18 +0700

    Update v4-[8].lua

--- PixelTowerDefense/v4-[8].lua
+++ PixelTowerDefense/v4-[8].lua
@@ -1,8 +1,14 @@
 shared.Configs = {
-        Enabled = true,
-        RedeemCode = true,
-        Speed = 2,  
-        GithubUrl = 'https://raw.githubusercontent.com/CodexHubX/CodeXPaidVersion/refs/heads/main/ConfigPTD.lua',
-        JobID = '',
-        TradingMode = false,
+    Enabled = true,
+    RedeemCode = true,
+    Speed = 2,  
+    GithubUrl = 'https://raw.githubusercontent.com/CodexHubX/CodeXPaidVersion/refs/heads/main/ConfigPTD.lua',
+    GithubConfigUrl = '',
+    JobID = '',
+    TradingMode = false,
+    AutoTrading = {
+        ['Enabled'] = false,
+        ['Main User'] = 'DummyDartSky',
+        ['JobID'] = '1fa91949-7917-4136-9e59-f9bf1f1fb2b3',
+    },
 };

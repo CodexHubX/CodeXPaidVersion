511358412c678f26a403d33c400bc044a7882508
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-10-17 01:33:08 +0700

    Update I7-1.lua

--- PixelTowerDefense/I7-1.lua
+++ PixelTowerDefense/I7-1.lua
@@ -7,7 +7,7 @@ shared.Configs = {
     JobID = '',
     TradingMode = false,
     AutoTrading = {
-        ['Enabled'] = true,
+        ['Enabled'] = false,
         ['Main User'] = 'GlowWormNewt',
         ['JobID'] = '8b2c854d-6f0e-4313-811e-896e86bdef0b',
     },

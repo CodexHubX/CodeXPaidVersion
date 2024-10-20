bb9001ad24ee24a7056c3d3ec9ed4e04d6eaee24
CodexHubX <111242276+CodexHubX@users.noreply.github.com>
2024-10-20 21:16:24 +0700

    Update v4-[7].lua

--- PixelTowerDefense/v4-[7].lua
+++ PixelTowerDefense/v4-[7].lua
@@ -6,6 +6,7 @@ shared.Configs = {
     GithubConfigUrl = 'https://raw.githubusercontent.com/CodexHubX/CodeXPaidVersion/refs/heads/main/PixelTowerDefense/v4-%5B7%5D.lua',
     JobID = '',
     TradingMode = false,
+    FreddyFarm = true,
     AutoTrading = {
         ['Enabled'] = false,
         ['Main User'] = 'MoonPinkScar',
